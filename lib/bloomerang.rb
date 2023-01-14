5f613e77f5f2d001ef01423806f2e5583304268f
Chip <chiperific@gmail.com>
2023-01-14 15:31:38 -0500

    Bloomerang::Address & updates to Bloomerang::Base

--- lib/bloomerang.rb
+++ lib/bloomerang.rb
@@ -6,6 +6,8 @@ require_relative "bloomerang/constituent"
 require_relative "bloomerang/transaction"
 require_relative "bloomerang/fund"
 
+require_relative "bloomerang/address"
+
 module Bloomerang
   class Error < StandardError; end
 end
