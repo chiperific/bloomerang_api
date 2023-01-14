b472efbf16cc8e2fbdbf2cebda3cabf1db87715c
Chip <chiperific@gmail.com>
2023-01-14 15:13:58 -0500

    Update campaign.rb
    post and put need blank params; unify comments with links to API

--- lib/bloomerang/campaign.rb
+++ lib/bloomerang/campaign.rb
@@ -4,6 +4,7 @@ require_relative "base"
 
 module Bloomerang
   ## Bloomerang::Campaign
+  ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns
   # Id        integer($int64)
   # Raised    number($currency)
   # The total amount raised for this campaign
@@ -16,6 +17,7 @@ module Bloomerang
   # IsActive  boolean
   class Campaign < Base
     ### Fetch all campaigns
+    ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns/get_campaigns
     #
     ## Params:
     # skip      integer, default: 0,  simple paging system
@@ -29,6 +31,7 @@ module Bloomerang
     end
 
     ### Show single campaign
+    ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns/get_campaign__id_
     #
     # Params:
     # id integer
@@ -37,23 +40,26 @@ module Bloomerang
     end
 
     ### Update single campaign
+    ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns/put_campaign__id_
     #
     ## Params:
     # id   integer
-    # body JSON object, see https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns
+    # body JSON object, see API for fields
     def update(id, body)
-      put("campaign/#{id}", body)
+      put("campaign/#{id}", {}, body)
     end
 
     ### Create single campaign
+    ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns/post_campaign
     #
     ## Params:
-    # body JSON object, see https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns
+    # body JSON object, see API for fields
     def create(body)
-      post("campaign", body)
+      post("campaign", {}, body)
     end
 
     ### Refresh summaries
+    ## https://bloomerang.co/product/integrations-data-management/api/rest-api/#/Campaigns/get_campaigns_refreshsummaries
     # Refreshes campaign goals
     # Returns: JSON: The list of campaigns that are active and have a non-zero goal.
     #
