d7f2e2d594e329739989fe33f5bd01a0af619fcd
Chip <chiperific@gmail.com>
2023-01-22 12:33:18 -0500

    Initializer location

--- lib/generators/bloomerang/initializer/initializer_generator.rb
+++ lib/generators/bloomerang/initializer/initializer_generator.rb
@@ -5,7 +5,7 @@ require "rails/generators/base"
 module Bloomerang
   module Generators
     class InitializerGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
 
       desc "Copy Bloomerang initializer file"
       def create_initializer_file
